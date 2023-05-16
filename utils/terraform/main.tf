a2d95745496ff674bab84e13f675fc5b65688906
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-05-16 11:59:23 +0200

    chore: update GitHub project terraform (#8)

--- utils/terraform/main.tf
+++ utils/terraform/main.tf
@@ -7,9 +7,12 @@ resource "github_repository" "annona-rs" {
   has_issues = true
   visibility = "public"
 
-  allow_rebase_merge = false
-  allow_merge_commit = false
+  allow_rebase_merge     = false
+  allow_merge_commit     = false
   delete_branch_on_merge = true
+
+  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_title   = "PR_TITLE"
 
   template {
     owner                = "bihealth"
