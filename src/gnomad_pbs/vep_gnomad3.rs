e08ce21825190f0a85a15aad2b6eb9be8874bc6d
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-05-23 07:29:14 +0200

    skip null domains

--- src/gnomad_pbs/vep_gnomad3.rs
+++ src/gnomad_pbs/vep_gnomad3.rs
@@ -81,11 +81,9 @@ impl FromStr for Vep {
                     let pairs = values[35].split('&').collect::<Vec<_>>();
                     pairs
                         .iter()
+                        .filter(|s| s != "null")
                         .map(|p| {
                             let tmp = p.split(':').collect::<Vec<_>>();
-                            if tmp.len() < 2 {
-                                dbg!(&values, &p);
-                            }
                             super::vep_common::Domain {
                                 source: tmp[0].to_string(),
                                 id: tmp[1].to_string(),
