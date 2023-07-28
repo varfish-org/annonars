fb0552337131829765d78b756aee2e18cd527093
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-07-28 09:00:01 +0200

    fixing docs

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -316,27 +316,27 @@ pub fn run(args_common: &common::cli::Args, args: &Args) -> Result<(), anyhow::E
         args.listen_port
     );
     tracing::info!(
-        "  try: http://{}:{}/genes/info?hgnc-id=HGNC:12403",
+        "  try: http://{}:{}/genes/info?hgnc_id=HGNC:12403",
         args.listen_host.as_str(),
         args.listen_port
     );
     tracing::info!(
-        "  try: http://{}:{}/annos/db-info?genome-release=grch37",
+        "  try: http://{}:{}/annos/db-info?genome_release=grch37",
         args.listen_host.as_str(),
         args.listen_port
     );
     tracing::info!(
-        "  try: http://{}:{}/annos/variant?genome-release=grch37&chromosome=1&pos=55505599&reference=C&alternative=G",
+        "  try: http://{}:{}/annos/variant?genome_release=grch37&chromosome=1&pos=55505599&reference=C&alternative=G",
         args.listen_host.as_str(),
         args.listen_port
     );
     tracing::info!(
-        "  try: http://{}:{}/annos/variant?genome-release=grch37&chromosome=1&pos=10001&reference=T&alternative=A",
+        "  try: http://{}:{}/annos/variant?genome_release=grch37&chromosome=1&pos=10001&reference=T&alternative=A",
         args.listen_host.as_str(),
         args.listen_port
     );
     tracing::info!(
-        "  try: http://{}:{}/annos/range?genome-release=grch37&chromosome=1&start=1&stop=55516888",
+        "  try: http://{}:{}/annos/range?genome_release=grch37&chromosome=1&start=1&stop=55516888",
         args.listen_host.as_str(),
         args.listen_port
     );
