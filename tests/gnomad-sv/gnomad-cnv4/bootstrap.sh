b011e5be175dfdb0836048459a0267cd62a5fb81
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-21 16:07:55 +0100

    feat: add multi-cohort support for gnomAD CNV v4

--- tests/gnomad-sv/gnomad-cnv4/bootstrap.sh
+++ tests/gnomad-sv/gnomad-cnv4/bootstrap.sh
@@ -9,22 +9,26 @@ trap "rm -rf $TMPDIR" EXIT
 
 rm -f $SCRIPT_DIR/*.vcf*
 
-curl \
-    https://storage.googleapis.com/gcp-public-data--gnomad/release/4.0/exome_cnv/gnomad.v4.0.cnv.all.vcf.gz \
-| zcat \
-| head -n 200 \
-> $SCRIPT_DIR/gnomad.v4.0.cnv.all.vcf
+for token in all non_neuro non_neuro_controls; do
+    curl \
+        https://storage.googleapis.com/gcp-public-data--gnomad/release/4.0/exome_cnv/gnomad.v4.0.cnv.$token.vcf.gz \
+    | zcat \
+    | head -n 200 \
+    > $SCRIPT_DIR/gnomad.v4.0.cnv.$token.vcf
 
-bgzip -c $SCRIPT_DIR/gnomad.v4.0.cnv.all.vcf \
-> $SCRIPT_DIR/gnomad.v4.0.cnv.all.vcf.gz
+    bgzip -c $SCRIPT_DIR/gnomad.v4.0.cnv.$token.vcf \
+    > $SCRIPT_DIR/gnomad.v4.0.cnv.$token.vcf.gz
 
-tabix -f $SCRIPT_DIR/gnomad.v4.0.cnv.all.vcf.gz
+    tabix -f $SCRIPT_DIR/gnomad.v4.0.cnv.$token.vcf.gz
 
-rm -rf $SCRIPT_DIR/rocksdb
+    rm -rf $SCRIPT_DIR/rocksdb
+done
 
 cargo run -- \
     gnomad-sv import \
     --path-in-vcf $SCRIPT_DIR/gnomad.v4.0.cnv.all.vcf.gz \
+    --path-in-vcf $SCRIPT_DIR/gnomad.v4.0.cnv.non_neuro.vcf.gz \
+    --path-in-vcf $SCRIPT_DIR/gnomad.v4.0.cnv.non_neuro_controls.vcf.gz \
     --path-out-rocksdb $SCRIPT_DIR/rocksdb \
     --gnomad-kind exomes \
     --genome-release grch38 \
